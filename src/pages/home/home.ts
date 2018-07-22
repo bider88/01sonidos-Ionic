84c5d15df3c5d87010a6fca5640c4187687de16f
Irving Didier <bider88@gmail.com>
2018-07-22 00:02:18 -0500

    Agregando los sonidos, ordenando listas y el pull refresh

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ANIMALES } from '../../data/data.animales';
 import { Animal } from '../../interfaces/animal.interface';
+import { Refresher, reorderArray } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -10,9 +11,57 @@ import { Animal } from '../../interfaces/animal.interface';
 export class HomePage {
 
   animales: Animal[] = [];
+  audio = new Audio();
+  audioTime: any;
+  order: boolean = false;
 
   constructor(public navCtrl: NavController) {
-    this.animales = ANIMALES.splice(0);
+    this.animales = ANIMALES.slice(0);
+  }
+
+  play(animal: Animal) {
+
+    this.pauseAudio(animal);
+
+    if (animal.reproduciendo) { animal.reproduciendo = false; return; }
+
+    this.audio = new Audio();
+
+    this.audio.src = animal.audio;
+
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+
+    this.audioTime = setTimeout( () => animal.reproduciendo = false, animal.duracion * 1000 );
+  }
+
+  delete( i: number) {
+    this.animales = this.animales.filter((animal, index) => index !== i);
+  }
+
+  doRefresh(e: Refresher) {
+
+    setTimeout(() =>  {
+      console.log('terminó el refresh');
+      this.animales = ANIMALES.slice(0);
+      e.complete();
+    }, 1000 )
+  }
+
+  private pauseAudio(animal: Animal) {
+    clearTimeout(this.audioTime);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (let animal of this.animales) {
+      if (animal.nombre !== animal.nombre) { animal.reproduciendo = false; }
+    }
+  }
+
+  reorderAnimal(index: any) {
+    this.animales = reorderArray(this.animales, index);
   }
 
 }
